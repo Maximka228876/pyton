b1af28f00feca7ab32a2ae074b90ba81d4c0cbd6
Maximka228876 <mlitvyanov@gmail.com>
2025-04-19 00:13:34 +0900

    Add files via upload

--- Ocylist/database/db.py
+++ Ocylist/database/db.py
@@ -1,23 +1,33 @@
-import sqlite3
+import os
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 
 def init_db():
-    conn = sqlite3.connect('bot_data.db')
+    # Подключение к PostgreSQL
+    conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cursor = conn.cursor()
+
+    # Создание таблиц
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY,
+            user_id BIGINT PRIMARY KEY,
             first_name TEXT,
             last_name TEXT,
             username TEXT,
             reg_date TEXT
-        )''')
+        )
+    ''')
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS reminders (
-            id INTEGER PRIMARY KEY,
-            user_id INTEGER,
+            id SERIAL PRIMARY KEY,
+            user_id BIGINT,
             text TEXT,
             time TEXT,
             active BOOLEAN
-        )''')
+        )
+    ''')
     conn.commit()
     conn.close()
