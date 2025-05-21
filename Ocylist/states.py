5ff97db641df8765c9969b4fc9fab6a23e351d8d
Maximka228876 <mlitvyanov@gmail.com>
2025-05-21 16:28:39 +0900

    states.py

--- Ocylist/states.py
+++ Ocylist/states.py
@@ -2,8 +2,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Form(StatesGroup):
     vision_test = State()
-    waiting_for_reminder_text = State()
-    waiting_for_reminder_time = State()
     waiting_for_feedback = State()
     color_test = State()
     astigmatism_test = State()
