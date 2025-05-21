a75a88d3455f74a7da0c29754f17118d0e0f1159
Maximka228876 <mlitvyanov@gmail.com>
2025-05-21 19:39:46 +0900

    main.py

--- Ocylist/main.py
+++ Ocylist/main.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 from config import dp, bot, scheduler
 from database.db import init_db
 
-# Импорт роутеров
 from handlers.diseases import router as diseases_router
 from handlers.start import router as start_router
 from handlers.vision import router as vision_router
@@ -18,17 +17,15 @@ from handlers.help import router as help_router
 load_dotenv()
 
 async def on_startup():
-    init_db()  # Создание таблиц
+    init_db()
     logging.info("Бот запущен")
 
 async def main():
-    # Настройка логирования
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
 
-    # Регистрация роутеров
     dp.include_router(diseases_router)
     dp.include_router(start_router)
     dp.include_router(vision_router)
@@ -38,15 +35,12 @@ async def main():
     dp.include_router(tips_router)
     dp.include_router(help_router)
 
-    # Запуск планировщика
     if not scheduler.running:
         scheduler.start()
         logging.info("Планировщик запущен")
 
-    # Стартовые задачи
     await on_startup()
 
-    # Запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
